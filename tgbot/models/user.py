7d199fd2a34d2c8d9d70b4a6bad43288cc5aefbe
Tim <herew26@gmail.com>
2023-03-31 02:12:51 +0300

    feat: update user model

--- tgbot/models/user.py
+++ tgbot/models/user.py
@@ -8,5 +8,7 @@ class User(TimedBaseModel):
     username = db.Column(db.String(32), nullable=True)
     first_name = db.Column(db.String(255), nullable=True)
     last_name = db.Column(db.String(255), nullable=True)
-    is_banned = db.Column(db.Boolean, nullable=False, default=False)
+    is_banned = db.Column(db.Boolean, nullable=False, server_default="False")
     deep_link = db.Column(db.String(225), nullable=True)
+    attempts = db.Column(db.Integer, nullable=False, server_default="0")
+    has_won = db.Column(db.Boolean, nullable=False, server_default="False")
